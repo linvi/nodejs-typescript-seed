e21546ffa10afc5feea25943b2add816085ad54b
linvi <linvi@users.noreply.github.com>
2016-12-06 04:06:17 +0000

    #5 mongoose repository now uses promise instead of callbacks

--- src/common/base.repository.ts
+++ src/common/base.repository.ts
@@ -4,34 +4,82 @@ export class RepositoryBase<T extends mongoose.Document> {
 
     private _model: mongoose.Model<mongoose.Document>;
 
-    constructor (schemaModel: mongoose.Model<mongoose.Document>) {
+    constructor(schemaModel: mongoose.Model<mongoose.Document>) {
         this._model = schemaModel;
     }
 
-    create (item: T, callback: (error: any, result: any) => void) {
-        this._model.create(item, callback);
+    create(item: T): Promise<T> {
+        const promise = new Promise((resolve, reject) => {
+            this._model.create(item, (error: any, result: any) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+
+        return promise;
     }
 
-    retrieve (callback: (error: any, result: any) => void) {
-        this._model.find({}, callback)
+    retrieve(): Promise<T[]> {
+        const promise = new Promise((resolve, reject) => {
+            this._model.find({}, (error: any, result: any) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+
+        return promise;
     }
 
-    update (_id: mongoose.Types.ObjectId, item: T, callback: (error: any, result: any) => void) {
-        this._model.update({_id: _id}, item, callback);
+    update(_id: mongoose.Types.ObjectId, item: T): Promise<T> {
+        const promise = new Promise((resolve, reject) => {
+            this._model.update({ _id: _id }, item, (error: any, result: any) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
 
+        return promise;
     }
 
-    delete (_id: string, callback:(error: any, result: any) => void) {
-        this._model.remove({_id: this.toObjectId(_id)}, (err) => callback(err, null));
+    delete(_id: string): Promise<void> {
+        const promise = new Promise((resolve, reject) => {
+            this._model.remove({ _id: this.toObjectId(_id) }, (error: any) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(null);
+                }
+            });
+        });
 
+        return promise;
     }
 
-    findById (_id: string, callback: (error: any, result: T) => void) {
-        this._model.findById( _id, callback);
+    findById(_id: string): Promise<T> {
+        const promise = new Promise((resolve, reject) => {
+            this._model.findById(_id, (error: any, result: any) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+
+        return promise;
     }
 
 
-    private toObjectId (_id: string) : mongoose.Types.ObjectId {
+    private toObjectId(_id: string): mongoose.Types.ObjectId {
         return mongoose.Types.ObjectId.createFromHexString(_id)
     }
 }
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -6,22 +6,18 @@ import * as express from "express";
 const _userRepository: UserRepository = new UserRepository();
 
 export class UserController {
-    
-    create(req: express.Request, res: express.Response): void {
-        try {
-            const user: IUserModel = <IUserModel>req.body;
-            _userRepository.create(user, (error, result) => {
-                if (error) {
-                    res.send(400, error);
-                } else {
-                    res.send(200, result);
-                }
-            });
 
-        } catch (e) {
-            Log.error(e);
-            res.send(400, 'The operation could not complete!');
-        }
+    create(req: express.Request, res: express.Response): void {
+        const user: IUserModel = <IUserModel>req.body;
+
+        _userRepository.create(user)
+            .then((user: IUserModel) => {
+                res.send(user);
+            })
+            .catch(error => {
+                Log.error(error);
+                res.send(400, 'Something went wrong!');
+            });
     }
 
     select(req: express.Request, res: express.Response): void {
